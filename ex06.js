0cc713dd8e08e62f7eee32fa826735f2e96afe0d
Julia_Chu <z3211232001@gmail.com>
2024-09-23 12:30:46 -0700

    completed 06

--- ex06.js
+++ ex06.js
@@ -1,7 +1,13 @@
 // Exercise 6: Write a function named 'uniqueValues' that takes an array of values and returns a new array with only unique values, removing duplicates.
 
 function uniqueValues(arr) {
-  // your code here
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (!newArr.includes(arr[i])) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
 }
 
-console.log(uniqueValues([1, 2, 2, 3, 4, 4, 5])) // [1, 2, 3, 4, 5]
+console.log(uniqueValues([1, 2, 2, 3, 4,4 ,6,4, 5])) // [1, 2, 3, 4, 5]
