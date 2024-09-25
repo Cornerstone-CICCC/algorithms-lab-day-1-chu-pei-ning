05dec3c4ae420ff1160b503167d2d45a2ba3977f
Julia_Chu <z3211232001@gmail.com>
2024-09-25 13:15:37 -0700

    completed 02

--- ex02.js
+++ ex02.js
@@ -1,7 +1,14 @@
 // Exercise 2: Write a function named 'filterByCategory' that takes an array of product objects and a category string, and returns a new array with products that belong to the given category.
 
 function filterByCategory(products, category) {
-  // your code here
+      let filtered = []
+      for(let i = 0; i < products.length; i++){
+            if (products[i].category === category) {
+                  filtered.push(products[i])
+            }
+      }
+      return filtered
 }
 
-console.log(filterByCategory([{ name: 'Apple', category: 'Fruit' }, { name: 'Carrot', category: 'Vegetable' }], 'Fruit')) // [{ name: 'Apple', category: 'Fruit' }]
+const array = [{ name: 'Apple', category: 'Fruit' }, { name: 'Carrot', category: 'Vegetable' }]
+console.log(filterByCategory(array, 'Fruit')) // [{ name: 'Apple', category: 'Fruit' }]
