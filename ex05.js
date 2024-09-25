d761b6296b66d542e761eac9df02946fdbac2b27
Julia_Chu <z3211232001@gmail.com>
2024-09-25 12:15:46 -0700

    completed 05

--- ex05.js
+++ ex05.js
@@ -1,7 +1,28 @@
 // Exercise 5: Write a function named 'groupByCategory' that takes an array of product objects and returns an object where the keys are categories and the values are arrays of products in those categories.
 
 function groupByCategory(products) {
-  // your code here
+  let output = {}
+  for (let i = 0; i < products.length; i++) {
+    let category = products[i].category
+    if (category in output) {
+      output[category].push(products[i])
+    } else {
+      output[category] = [products[i]]
+    }
+  }
+  return output
 }
 
-console.log(groupByCategory([{ name: 'Apple', category: 'Fruit' }, { name: 'Carrot', category: 'Vegetable' }, { name: 'Banana', category: 'Fruit' }])) // { Fruit: [{ name: 'Apple', category: 'Fruit' }, { name: 'Banana', category: 'Fruit' }], Vegetable: [{ name: 'Carrot', category: 'Vegetable' }] }
+
+const array = [
+  { name: 'Apple', category: 'Fruit' }, 
+  { name: 'Carrot', category: 'Vegetable' }, 
+  { name: 'Banana', category: 'Fruit' }
+]
+console.log(groupByCategory( array)) 
+/*
+{ 
+  Fruit: [{ name: 'Apple', category: 'Fruit' }, { name: 'Banana', category: 'Fruit' }], 
+  Vegetable: [{ name: 'Carrot', category: 'Vegetable' }] 
+}
+*/
