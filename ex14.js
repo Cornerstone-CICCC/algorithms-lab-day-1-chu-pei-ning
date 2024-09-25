0e3e974ab1189cedb6b997098757451818d84385
Julia_Chu <z3211232001@gmail.com>
2024-09-25 15:15:29 -0700

    completed 14

--- ex14.js
+++ ex14.js
@@ -1,7 +1,16 @@
 // Exercise 14: Write a function named 'mergeArrays' that takes two arrays and returns a new array that combines the elements of both arrays, alternating between them.
 
 function mergeArrays(arr1, arr2) {
-  // your code here
+  let newArr = []
+  for (let i = 0; i < arr1.length; i++) {
+    newArr.push(arr1[i])
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    newArr.push(arr2[i])
+  }
+  newArr.sort((a,b) => a - b)
+
+  return newArr
 }
 
 console.log(mergeArrays([1, 3, 5], [2, 4, 6])) // [1, 2, 3, 4, 5, 6]
