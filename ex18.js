bcf9403300f1bf80189a5ee731a0cc60b8658fb2
Julia_Chu <z3211232001@gmail.com>
2024-09-25 16:40:55 -0700

    completed 18

--- ex18.js
+++ ex18.js
@@ -3,8 +3,12 @@
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 
 function fibonacci(n) {
-  // your code here
+  if (n <= 1) {
+    return n
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2)
 }
+
 
 console.log(fibonacci(5)); // Output: 5
 console.log(fibonacci(10)); // Output: 55
