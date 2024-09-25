9193788956eeefca9eaa939074d49c6cbe7f7ecd
Julia_Chu <z3211232001@gmail.com>
2024-09-25 15:27:59 -0700

    completed 15

--- ex15.js
+++ ex15.js
@@ -1,7 +1,14 @@
 // Exercise 15: Write a function named 'countWords' that takes a string of text and returns the number of words in the string.
 
 function countWords(text) {
-  // your code here
+  let word = text.split(" ")
+  const noEmptyWord = word.filter( space => {
+    if(space !== " "){
+      return space
+    }
+  })
+  let output = noEmptyWord.length
+  return output
 }
 
 console.log(countWords('The quick brown fox jumps over the lazy dog')) // 9
