ed13a77459eef2df7302eef3babf66dffc08a1b7
Julia_Chu <z3211232001@gmail.com>
2024-09-25 15:40:34 -0700

    completed 16

--- ex16.js
+++ ex16.js
@@ -1,7 +1,18 @@
 // Exercise 16: Write a function named 'sumNestedArray' that takes an array of arrays of numbers and returns the sum of all numbers.
 
 function sumNestedArray(arr) {
-  // your code here
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if(Array.isArray(arr[i])) {
+      newArr = newArr.concat(sumNestedArray(arr[i]))
+    } else {
+      newArr.push(arr[i])
+    }
+  }
+  const sum = newArr.reduce((total, currentElement) => {
+    return total + currentElement
+  }, 0 )
+  return sum
 }
 
 console.log(sumNestedArray([[1, 2, 3], [4, 5], [6, 7, 8]])) // 36
