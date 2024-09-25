6a58895d7ef4ec1b9cd0593ca4108b11fd52c6ad
Julia_Chu <z3211232001@gmail.com>
2024-09-25 13:23:28 -0700

    completed 11

--- ex11.js
+++ ex11.js
@@ -1,7 +1,9 @@
 // Exercise 11: Write a function named 'reverseWords' that takes a string of text and returns the string with the order of the words reversed.
 
 function reverseWords(text) {
-  // your code here
+  let words = text.split(" ")
+  let newWords = words.reverse()
+  return newWords.join(" ")
 }
 
 console.log(reverseWords('The quick brown fox')) // 'fox brown quick The'
