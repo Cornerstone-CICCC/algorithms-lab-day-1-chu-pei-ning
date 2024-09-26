5030ebb372348a6aa0b979f923d8e31104749512
Julia_Chu <z3211232001@gmail.com>
2024-09-26 13:16:16 -0700

    completed 17

--- ex17.js
+++ ex17.js
@@ -6,7 +6,16 @@ function mostFrequent(arr) {
     return property
   }, {} ) //出來是{ apple: 3, banana: 2, orange: 1 }
 
-  return 
+  let friuts = [];
+
+  for (let friut in fruitCounter) {
+      friuts.push({name: friut, count: fruitCounter[friut]})
+  }
+
+  friuts.sort((a, b) => b.count - a.count);
+
+  return friuts[0].name
+
 }
 
 console.log(mostFrequent(['apple', 'banana', 'apple', 'orange', 'banana', 'apple'])) // 'apple'
