7a3973e6bb89e7572b43585085d79f60dfcda84d
Julia_Chu <z3211232001@gmail.com>
2024-09-25 20:59:56 -0700

    haven't finish

--- ex17.js
+++ ex17.js
@@ -1,7 +1,12 @@
 // Exercise 17: Write a function named 'mostFrequent' that takes an array of strings and returns the string that appears the most frequently.
 
 function mostFrequent(arr) {
-  // your code here
+  const fruitCounter = arr.reduce((property, current) => {
+    property[current] = (property[current] || 0) + 1
+    return property
+  }, {} ) //出來是{ apple: 3, banana: 2, orange: 1 }
+
+  return 
 }
 
 console.log(mostFrequent(['apple', 'banana', 'apple', 'orange', 'banana', 'apple'])) // 'apple'
