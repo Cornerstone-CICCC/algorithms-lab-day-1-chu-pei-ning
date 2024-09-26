22a46fa925515e86d2f28cc955aad0e4207d13e1
Julia_Chu <z3211232001@gmail.com>
2024-09-26 11:11:07 -0700

    updated 18

--- ex18.js
+++ ex18.js
@@ -2,11 +2,18 @@
 // Fibonacci sequence: The Fibonacci sequence is a series of numbers in which each number (called a Fibonacci number) is the sum of the two preceding ones. Typically, the sequence starts with 0 and 1. The sequence goes as follows:
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 
-function fibonacci(n) {
-  if (n <= 1) {
-    return n
-  }
-  return fibonacci(n - 1) + fibonacci(n - 2)
+// function fibonacci(n) {
+//   if (n <= 1) {
+//     return n
+//   }
+//   return fibonacci(n - 1) + fibonacci(n - 2)
+// }
+
+function fibonacci(num) {
+	if(num <= 1 ){
+		return num
+	}
+	return fibonacci(num - 2) + fibonacci(num - 1)
 }
 
 
