ee0575ed1bcf82967168b6158f0e5243fc1718d1
Julia_Chu <z3211232001@gmail.com>
2024-09-30 12:01:45 -0700

    completed 19

--- ex19.js
+++ ex19.js
@@ -1,7 +1,19 @@
 // Exercise 19: Write a function named 'flattenObject' that takes a nested object and returns a new object with no nested properties.
 
 function flattenObject(obj) {
-  // your code here
+  return recursive(obj, '', {})
 }
+function recursive(obj, parent, result) { 
+  for (let propety in obj) {
+    let propName = parent ? `${parent}.${propety}` : propety 
+    if (typeof(obj[propety]) == 'object') {
+      recursive(obj[propety], propName, result)
+    } else {
+      result[propName] = obj[propety]
+    }
+  }
+  return result
+}
+
 
 console.log(flattenObject({ a: { b: { c: 1 } }, d: 2 })) // { 'a.b.c': 1, d: 2 }
