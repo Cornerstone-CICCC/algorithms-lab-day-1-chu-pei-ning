fa8ff618421b39993372d3b1b84f2c637d50f4e5
Julia_Chu <z3211232001@gmail.com>
2024-09-30 12:40:11 -0700

    completed 20

--- ex20.js
+++ ex20.js
@@ -1,7 +1,19 @@
 // Exercise 20: Write a function named 'anagramGroups' that takes an array of strings and returns an array of arrays, grouping anagrams together.
 
 function anagramGroups(arr) {
-  // your code here
+  let refObj = {}
+  let result = []
+
+  for (let item of arr) {
+    let key = item.split('').sort().join('')
+    if (refObj.hasOwnProperty(key)){
+      refObj[key].push(item)
+      result.push(refObj[key])
+    } else {
+      refObj[key] = [item]
+    }
+  }
+  return result
 }
 
 console.log(anagramGroups(['bat', 'tab', 'cat', 'act'])) // [['bat', 'tab'], ['cat', 'act']]
